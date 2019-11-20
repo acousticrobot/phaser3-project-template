9f2a0308e08d4a4b6c9add32206a85368c17cef9
Jonathan Gabel <hello@jonathangabel.com>
2019-11-20 16:38:21 -0500

    initial changes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import Phaser from "phaser";
 import logoImg from "./assets/logo.png";
 
+const docElement = document.documentElement
+const width = docElement.clientWidth > 800 ?
+  docElement.clientWidth : 800
+const height = docElement.clientHeight > 600 ?
+  docElement.clientHeight : 600
+
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: width,
+  height: height,
   scene: {
     preload: preload,
     create: create
@@ -23,7 +29,7 @@ function create() {
 
   this.tweens.add({
     targets: logo,
-    y: 450,
+    y: height - 150,
     duration: 2000,
     ease: "Power2",
     yoyo: true,
